5b3c36dd100832bef39d02632f638792a4ecb0b2
LoicGrapp <loic.hollay@outlook.com>
2025-12-02 11:51:47 +0100

    added the about page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.app')
+@section('content')
+    <h1>About</h1>
+    <p>This is the about page of our Laravel application. Here, you can find information about the purpose and background of this project.</p>
+    <p>Our Laravel application is designed to demonstrate basic routing, views, and controller functionalities
+@endsection
