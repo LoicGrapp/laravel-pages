b9f6c4ec36dca070e9dc5729a902c75860870a85
LoicGrapp <loic.hollay@outlook.com>
2025-12-02 11:48:47 +0100

    - Created the homepages and route for it

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.app')
+@section('content')
+    <h1>Accueil</h1>
+    <p>Welcome to the homepage of our Laravel application! This application is a test for my classes LPW. I hope everything is doing well for everybody</p>
+    <p>This page is made by calling the content on the layout page called app.blade.php</p>
+@endsection
